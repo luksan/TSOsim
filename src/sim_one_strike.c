e0638e0a76cc5973f145b006b05aaa473a7d3e94
Lukas Sandström <luksan@gmail.com>
2013-06-24 12:22:46 +0200

    C: fix bug in kill_defenders(), and simplify code.

--- src/sim_one_strike.c
+++ src/sim_one_strike.c
@@ -30,7 +30,7 @@ parray_t attack_one_defender(attacker_t A, defender_t D);
 plist_t * kill_defenders(attacker_t A, defender_t D);
 static plist_t * for_each_sub_sim(plist_t *p1, attacker_t A, defender_t D, double p_in);
 static int sure_kills(attacker_t A, defender_t D);
-static int attack_one_defender1(const int n_sure_kill, attacker_t A, const int hp, parray_t parr);
+static void attack_one_defender1(const int n_sure_kill, attacker_t A, const int hp, parray_t parr);
 plist_t * sim_sub(attacker_t A, defender_t D, double p_in);
 
 typedef unsigned long ULONG;
@@ -46,22 +46,18 @@ plist_t * sim(attacker_t A, defender_t D)
 plist_t * sim_sub(attacker_t A, defender_t D, double p_in)
 {
 	int sk, i;
-	plist_t *p1, *p2, *p_out = NULL;
-	if (A->n == 0 || D->n == 0) {
-		p_out = new_plist();
-		set_p_element(p_out, A->n, D->n, D->hp_remaining, p_in, NULL);
-		return p_out;
-	}
+	plist_t *p1 = NULL, *p2;
+	if (A->n == 0 || D->n == 0)
+		return set_p_element(new_plist(), A->n, D->n, D->hp_remaining, p_in, NULL);
+
 	sk = sure_kills(A, D);
 	if (sk == 0) { // no sure kills remaining
 		parray_t pa = attack_one_defender(A, D);
-		for (i = 0; i < pa->len; i++) {
-			if (!pa->a[i]) // uninitialized element
-				continue;
-			p_out = plist_merge(p_out, for_each_sub_sim(pa->a[i], A, D, p_in));
-		}
+		for (i = 0; i < pa->len; i++)
+			if (pa->a[i])
+				p1 = plist_merge(p1, for_each_sub_sim(pa->a[i], A, D, p_in));
 		parray_free(pa);
-		return p_out;
+		return p1;
 	}
 	p1 = kill_defenders(A, D);
 	for (p2 = p1; p2; p2 = p2->next)
@@ -98,18 +94,16 @@ static int min_attackers(int dmg_min, int hp)
 		return 500000;
 	return ceil(hp/(float)dmg_min);
 }
-static int min(int a, int b)
+
+static inline int min(int a, int b)
 {
-	if (a < b)
-		return a;
-	return b;
+	return a < b ? a : b;
 }
-static int max(int a, int b)
+static inline int max(int a, int b)
 {
-	if (a > b)
-		return a;
-	return b;
+	return a > b ? a : b;
 }
+
 static int sure_kills(const attacker_t A, const defender_t D)
 {
 	return min(D->n, A->n/min_attackers(A->dmg_min, D->hp));
@@ -147,19 +141,18 @@ plist_t * kill_defenders(attacker_t A, defender_t D)
 		}
 	}
 	parray_free(kill_prob);
+
 	p1 = calloc(p_alloc, sizeof(double));
-	p2 = calloc(p_alloc, sizeof(double));
-	memset(p1, 0, p_alloc*sizeof(double));
+	p2 = malloc(p_alloc*sizeof(double));
 	for (i = 0; i < p0_len; i++)
 		p1[p0[i].n] = p0[i].p;
 
 	for (i = 1; i < dloss; i++) { // multiply polynomials
 		memset(p2, 0, p_alloc*sizeof(double));
-		for (p1_i = 0; p1_i < p_alloc; p1_i++) {
+		for (p1_i = 0; p1_i < p_alloc; p1_i++)
 			for (j = 0; j < p0_len; j++)
 				if (p1[p1_i] != 0)
-					p2[p1_i+p0[j].n] = p0[j].p * p1[p1_i];
-		}
+					p2[p1_i+p0[j].n] += p0[j].p * p1[p1_i];
 		SWAP_PTRS(p1, p2);
 	}
 	plist_t *p_out = NULL;
@@ -235,7 +228,7 @@ static double outcome_prob(int hit, int miss, attacker_t A, int hp)
 	return exp(B+H+M);
 }
 
-static int attack_one_defender1(const int n_sure_kill, attacker_t A, const int hp, parray_t parr)
+static void attack_one_defender1(const int n_sure_kill, attacker_t A, const int hp, parray_t parr)
 {
 	int hit = 0, dmg, miss;
 	double p;
@@ -252,11 +245,10 @@ static int attack_one_defender1(const int n_sure_kill, attacker_t A, const int h
 		} else {
 			parray_incr_p(hit+miss, 1, 0, p, parr);
 			if (miss == 0)
-				return 1;
+				return;
 			miss--;
 		}
 	}
-	return 1;
 }
 
 static ULONG binomial(int n, int k)
@@ -290,12 +282,16 @@ static ULONG multinomial(int n, int k[], int k_len)
 
 void print_plist(plist_t *p)
 {
-	for(;p;p = p->next)
+	double p_tot = 0;
+	for(;p;p = p->next) {
+		p_tot += p->p;
 		printf("%3i, %3i, %3i, %.3e\n",
 			p->Na,
 			p->Nd,
 			p->hp_remaining,
 			p->p);
+	}
+	printf("Total prob: %f\n", p_tot);
 }
 void print_parray(parray_t pa)
 {
